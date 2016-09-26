9ee0c6d6008c5d80e3ca96f893dd2071df31b546
Bastiaan van den Berg <b.a.vandenberg@gmail.com>
2016-09-26 14:15:18 +0200

    Show outputs opened, closed, selected, and deselected.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
@@ -14,12 +14,19 @@ import {FormControl, FormGroup} from '@angular/forms';
 		[multiple]="multiple0"
 		placeholder="Select one"
         formControlName="selectSingle"
-		allowClear="true">
+		allowClear="true"
+        (opened)="onSingleOpened()"
+        (closed)="onSingleClosed()"
+        (selected)="onSingleSelected($event)"
+        (deselected)="onSingleDeselected($event)">
 	</ng-select>
     
     <div style="margin:10px 0 20px 0;color:#666;font-size:11pt;font-style:italic;">
 	    Selected option id: {{form.value['selectSingle']}}
     </div>
+
+    <div>Events:</div>
+    <pre #preSingle>{{logSingleString}}</pre>
 
     <hr style="margin: 18px 0;">
 
@@ -28,12 +35,19 @@ import {FormControl, FormGroup} from '@angular/forms';
 		[options]="options1"
 		[multiple]="multiple1"
 		placeholder="Select multiple"
-        formControlName="selectMultiple">
+        formControlName="selectMultiple"
+        (opened)="onMultipleOpened()"
+        (closed)="onMultipleClosed()"
+        (selected)="onMultipleSelected($event)"
+        (deselected)="onMultipleDeselected($event)">
 	</ng-select>
 
     <div style="margin:10px 0 20px 0;color:#666;font-size:11pt;font-style:italic;">
 	    Selected option id: {{form.value['selectMultiple']}}
     </div>
+
+    <div>Events:</div>
+    <pre #preMultiple>{{logMultipleString}}</pre>
 </form>`
 })
 
@@ -46,6 +60,12 @@ export class AppComponent implements OnInit {
     options0: Array<any> = [];
     options1: Array<any> = [];
     selection: Array<string>;
+
+    @ViewChild('preSingle') preSingle;
+    @ViewChild('preMultiple') preMultiple;
+
+    logSingleString: string = '';
+    logMultipleString: string = '';
 
     constructor() {
 
@@ -67,6 +87,64 @@ export class AppComponent implements OnInit {
         this.form = new FormGroup({});
         this.form.addControl('selectSingle', new FormControl(''));
         this.form.addControl('selectMultiple', new FormControl(''));
+    }
+
+    onSingleOpened() {
+        this.logSingle('- opened');
+    }
+
+    onSingleClosed() {
+        this.logSingle('- closed');
+    }
+
+    onSingleSelected(item) {
+        this.logSingle('- selected (value: ' + item.value  + ', label:' + 
+                       item.label + ')');
+    }
+
+    onSingleDeselected(item) {
+        this.logSingle('- deselected (value: ' + item.value  + ', label:' + 
+                       item.label + ')');
+    }
+
+    onMultipleOpened() {
+        this.logMultiple('- opened');
+    }
+
+    onMultipleClosed() {
+        this.logMultiple('- closed');
+    }
+
+    onMultipleSelected(item) {
+        this.logMultiple('- selected (value: ' + item.value  + ', label:' + 
+                       item.label + ')');
+    }
+
+    onMultipleDeselected(item) {
+        this.logMultiple('- deselected (value: ' + item.value  + ', label:' + 
+                       item.label + ')');
+    }
+
+    private logSingle(msg: string) {
+        this.logSingleString += msg + '\n';
+        
+        // Let change detection do its work before scrolling to div bottom.
+        setTimeout(() => {
+            this.scrollToBottom(this.preSingle.nativeElement);
+        });
+    }
+
+    private logMultiple(msg: string) {
+        this.logMultipleString += msg + '\n';
+
+        // Let change detection do its work before scrolling to div bottom.
+        setTimeout(() => {
+            this.scrollToBottom(this.preMultiple.nativeElement);
+        });
+    }
+
+    private scrollToBottom(elem) {
+        elem.scrollTop = elem.scrollHeight;
     }
 }
 
