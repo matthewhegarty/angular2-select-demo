fd358be6be93100b2bfe430f36092b882a327d10
Bastiaan van den Berg <b.a.vandenberg@gmail.com>
2016-10-10 16:54:37 +0200

    Separate forms single and multiple, add buttons per form.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,13 +5,14 @@ import {FormControl, FormGroup} from '@angular/forms';
     selector: 'angular2-select-demo-app',
     template: `
 <h1>Angular 2 select demo app</h1>
+
 <form style="padding:18px;max-width:800px;"
-    [formGroup]="form">
+    [formGroup]="formSingle">
 	
     <div style="margin:5px 0;font-weight:600;">Single select example</div>
     <ng-select
-		[options]="options0"
-		[multiple]="multiple0"
+		[options]="optionsSingle"
+		[multiple]="multipleSingle"
 		placeholder="Select one"
         formControlName="selectSingle"
 		allowClear="true"
@@ -22,18 +23,26 @@ import {FormControl, FormGroup} from '@angular/forms';
 	</ng-select>
     
     <div style="margin:10px 0 20px 0;color:#666;font-size:11pt;font-style:italic;">
-	    Selected option id: {{form.value['selectSingle']}}
+	    Selected option id: {{formSingle.value['selectSingle']}}
     </div>
 
     <div>Events:</div>
     <pre #preSingle>{{logSingleString}}</pre>
 
-    <hr style="margin: 18px 0;">
+</form>
+
+<button
+    (click)="onSingleResetClick()">Reset</button>
+
+<hr style="margin: 18px 0;">
+
+<form style="padding:18px;max-width:800px;"
+    [formGroup]="formMultiple">
 
     <div style="margin:5px 0;font-weight:600;">Multilpe select example</div>
 	<ng-select
-		[options]="options1"
-		[multiple]="multiple1"
+		[options]="optionsMultiple"
+		[multiple]="multipleMultiple"
 		placeholder="Select multiple"
         formControlName="selectMultiple"
         (opened)="onMultipleOpened()"
@@ -43,24 +52,29 @@ import {FormControl, FormGroup} from '@angular/forms';
 	</ng-select>
 
     <div style="margin:10px 0 20px 0;color:#666;font-size:11pt;font-style:italic;">
-	    Selected option id: {{form.value['selectMultiple']}}
+	    Selected option id: {{formMultiple.value['selectMultiple']}}
     </div>
 
     <div>Events:</div>
     <pre #preMultiple>{{logMultipleString}}</pre>
-</form>`
-})
+</form>
+    
+<button
+    (click)="onMultipleResetClick()">Reset</button>
+`})
 
 export class AppComponent implements OnInit {
 
-    form: FormGroup;
+    formSingle: FormGroup;
+    multipleSingle: boolean = false;
+    optionsSingle: Array<any> = [];
+    initialValueSingle: string = '22';
 
-    multiple0: boolean = false;
-    multiple1: boolean = true;
-    options0: Array<any> = [];
-    options1: Array<any> = [];
-    selection: Array<string>;
-
+    formMultiple: FormGroup;
+    multipleMultiple: boolean = true;
+    optionsMultiple: Array<any> = [];
+    initialValueMultiple: Array<string> = ['2', '22', '66'];
+    
     @ViewChild('preSingle') preSingle;
     @ViewChild('preMultiple') preMultiple;
 
@@ -79,14 +93,18 @@ export class AppComponent implements OnInit {
             };
         }
 
-        this.options0 = opts.slice(0);
-        this.options1 = opts.slice(0);
+        this.optionsSingle = opts.slice(0);
+        this.optionsMultiple = opts.slice(0);
     }
 
     ngOnInit() {
-        this.form = new FormGroup({});
-        this.form.addControl('selectSingle', new FormControl(''));
-        this.form.addControl('selectMultiple', new FormControl(''));
+        this.formSingle = new FormGroup({});
+        this.formSingle.addControl('selectSingle', 
+                new FormControl(this.initialValueSingle));
+
+        this.formMultiple = new FormGroup({});
+        this.formMultiple.addControl('selectMultiple', 
+                new FormControl(this.initialValueMultiple));
     }
 
     onSingleOpened() {
@@ -123,6 +141,14 @@ export class AppComponent implements OnInit {
     onMultipleDeselected(item) {
         this.logMultiple('- deselected (value: ' + item.value  + ', label:' + 
                        item.label + ')');
+    }
+
+    onSingleResetClick() {
+        this.formSingle.reset();
+    }
+
+    onMultipleResetClick() {
+        this.formMultiple.reset();
     }
 
     private logSingle(msg: string) {
